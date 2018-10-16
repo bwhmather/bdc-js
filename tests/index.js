ca1beeb387418de669b2bb456561072e38cdce5b
Ben Mather <bwhmather@bwhmather.com>
2018-10-16 19:21:52 +0100

    Add example test using testcafe

---
+++ tests/index.js
@@ -0,0 +1,18 @@
+import { Selector } from 'testcafe';
+
+fixture("Boilerplate").page("./fixture.html")
+
+test("Test hello world", async t => {
+  await t.eval(() => {
+    bdc.render(
+      document.getElementById("root"),
+      "Hello, World!",
+    );
+  });
+
+  const $root = Selector('#root')
+
+  await t.expect($root.textContent).eql("Hello, World!");
+  await t.expect($root.hasChildElements).eql(false);
+  await t.expect($root.childNodeCount).eql(1);
+});

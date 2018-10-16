7299b2bcfb140472d67a751bacc0e21a36c80ba1
Ben Mather <bwhmather@bwhmather.com>
2018-10-16 20:47:27 +0100

    Add test for creating an empty div

--- tests/index.js
+++ tests/index.js
@@ -16,3 +16,19 @@ test("Hello world", async t => {
   await t.expect($root.hasChildElements).eql(false);
   await t.expect($root.childNodeCount).eql(1);
 });
+
+test("Empty div", async t => {
+  await t.eval(() => {
+    bdc.render(
+      document.getElementById('root'),
+      bdc.h('div', {}),
+    );
+  });
+
+  const $root = Selector('#root');
+
+  await t.expect($root.childElementCount).eql(1);
+  await t.expect($root.child(0).tagName).eql('div');
+  await t.expect($root.child(0).hasChildNodes).eql(false);
+  await t.expect($root.child(0).attributes).eql({});
+});

bb6bdd785093c06e4cb9152b38952196f26803c0
Ben Mather <bwhmather@bwhmather.com>
2018-11-27 20:50:28 +0100

    Add docstrings for majority of functions.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-// The dom doesn't provide a way to list all registered event handlers, and
+// The DOM doesn't provide a way to list all registered event handlers, and
 // even if it did, there wouldn't be an easy way to figure out which belonged
 // to us.  Instead we maintain our own global map of event handlers to keep
 // track of what we've registered.
+// `EVENT_HANDLER_MAP`` is a weak mapping from HTML elements to dictionaries
+// mapping from event names to the functions BDC should call in response to
+// them.
 const EVENT_HANDLER_MAP = new WeakMap();
 
 function handleEvent(evt) {
@@ -14,8 +17,27 @@ function handleEvent(evt) {
   }
 }
 
+/**
+ * Sets an attribute or event handler on an HTML element to the requested
+ * value.
+ *
+ * The behaviour is quite attribute specific, but is intended to allow node
+ * descriptions to more closely resemble HTML-as-text instead of HTML as DOM.
+ * Calling `$elem.getAttribute(key)` will often return a deserialized
+ * equivalent of value or, as in the case of event handlers, nothing at all.
+ *
+ * @param $elem
+ *   The HTML element to set the attribute on.
+ * @param key
+ *   The name of the attribute or event handler to set.
+ * @param value
+ *   A description of the value to set.
+ */
 function setAttribute($elem, key, value) {
   if (key[0] === "o" && key[1] === "n") {
+    // The attribute is an event handler.  Unfortunately the DOM doesn't
+    // provide any way to list event handlers, or to remove event handlers by
+    // name.  See documentation on `EVENT_HANDLER_MAP`.
     let handlers = EVENT_HANDLER_MAP.get($elem);
     if (typeof handlers === "undefined") {
       $elem.addEventListener(key.slice(2), handleEvent, false);
@@ -65,16 +87,31 @@ function setAttribute($elem, key, value) {
   }
 }
 
+/**
+ * Removes or unsets the requested attribute on an HTML element.
+ *
+ * Does not guarantee good behaviour if the attribute is not one of those
+ * returned by `listAttributes`.
+ *
+ * @param $elem
+ *   The HTML element to remove the attribute from.
+ * @param key
+ *   The name of the attribute or event handler to remove, as it would appear
+ *   in an HTML document.
+ */
 function removeAttribute($elem, key) {
   if (key[0] === "o" && key[1] === "n") {
     const handlers = EVENT_HANDLER_MAP.get($elem);
     if (typeof handlers !== "undefined") {
-      if (handlers.hasOwnProperty[key.slice(2)]) {
-        $elem.removeEventListener(key.slice(2), handlers[key.slice(2)], false);
-        delete handlers[key.slice(2)];
+      const eventName = key.slice(2);
+      if (handlers.hasOwnProperty[eventName]) {
+        $elem.removeEventListener(eventName, handlers[eventName], false);
+        delete handlers[eventName];
       }
 
       if (Object.keys(handlers).length === 0) {
+        // BDC no longer has any handlers linked listening for events from this
+        // element.  We can clean up the empty entry.
         EVENT_HANDLER_MAP.delete(key.slice(2));
       }
     }
@@ -94,6 +131,16 @@ function removeAttribute($elem, key) {
   }
 }
 
+/**
+ * Returns a list of all attributes of an HTML element, including event
+ * handlers managed by BDC, but not including event handlers added by other
+ * libraries.
+ *
+ * @param $elem
+ *   The HTML element to list the attributes of.
+ * @returns
+ *   A list of attribute names.
+ */
 function listAttributes($elem) {
   const attributes = [];
   // tslint:disable-next-line
@@ -135,7 +182,21 @@ function updateChildren($elem, children) {
   }
 }
 
-// Renders a node to an element and returns it.
+/**
+ * Creates, replaces, or updates an HTML element to match a node description.
+ *
+ * @param node
+ *   A description of the target state of the HTML element.
+ * @param $parent
+ *   The HTML element that the updated element should be a child of.  This is a
+ *   required argument.
+ * @param $elem
+ *   The element currently sitting at the location that should be updated to
+ *   match the node description.  If `null`, it is assumed that the node
+ *   describes a new child that should be appended to the current list.  If the
+ *   type of the element does not match the node type it will be replaced
+ *   rather than updated.
+ */
 function update(node, $parent, $elem) {
   const $original = $elem;
 
@@ -162,10 +223,32 @@ function update(node, $parent, $elem) {
   }
 }
 
+/**
+ * Creates a node object describing a regular HTML element.
+ *
+ * @param type
+ *   The element tag name.
+ * @param attributes
+ *   A non-optional mapping from attribute names to target state values.
+ * @param children
+ *   An arbitrary number of node objects or strings describing the children of
+ *   this node.
+ */
 export function h(type, attributes, ...children) {
   return { type, attributes, children };
 }
 
+/**
+ * Replaces or updates the children of `$root` to match the descriptions passed
+ * as positional arguments.
+ *
+ * @param $root
+ *   A reference to an HTML element, the children of which should be updated.
+ *
+ * @param children
+ *   An arbitrary number of node objects or strings describing the desired
+ *   state of the children of this element.
+ */
 export function render($root, ...nodes) {
   updateChildren($root, nodes);
 }
